39ef821cc8ab179c9c8e2b7bf8d77b566bc66071
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2025-02-23 23:24:08 -0300

    desafio app biscoito da sorte finalizado

--- App.js
+++ App.js
@@ -17,21 +17,29 @@ export default function App() {
 
     function quebraBiscoito() {
         let numeroAleatoio = Math.floor(Math.random() * frases.length)
-        setTextoFrase(frases[numeroAleatoio])
+        setTextoFrase(`" ${frases[numeroAleatoio]} "`)
+        setImage(require('./src/biscoitoAberto.png'))
         
+    }
+
+    function reiniciar() {
+        setImage(require('./src/biscoito.png'))
+        setTextoFrase('')
     }
     
     return (
         <View style={styles.container}>
+
             <Image source={img} style={styles.img} />
             <Text style={styles.textofrase}>{textoFrase}</Text>
+
             <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
                 <View style={styles.btnArea}>
                     <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
                 </View>
             </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.botao, {marginTop:15,borderColor:'#121212'}]} onPress={()=> alert('teste')}>
+        <TouchableOpacity style={[styles.botao, {marginTop:15,borderColor:'#121212'}]} onPress={reiniciar}>
                 <View style={styles.btnArea}>
                     <Text style={[styles.btnTexto, {color:'#121212'}]}>Reiniciar Biscoito</Text>
                 </View>
